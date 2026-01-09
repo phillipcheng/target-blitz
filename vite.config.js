a2c2545870b862b53c927f416838e1540a53e36f
Cheng Yi <phillipchengyi@gmail.com>
2026-01-09 14:18:06 -0800

    Update vite config for iOS builds

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/target-blitz/',
+  base: process.env.GITHUB_PAGES ? '/target-blitz/' : '/',
   server: {
     host: true,
     port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
